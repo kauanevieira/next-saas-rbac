4babc5818640db36c85507fbf6222575ab79d994
Kauane Vieira <kauaneviieira@gmail.com>
2025-09-14 16:10:33 -0300

    setup do shadcn/ui

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,7 +1,7 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <h1>Hello Word</h1>
+    <Button>Sign in</Button>
   );
 }
