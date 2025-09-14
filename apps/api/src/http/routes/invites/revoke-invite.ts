5c70c34cf243437f787245be0c9c4e748fceb2c7
Kauane Vieira <kauaneviieira@gmail.com>
2025-09-14 15:21:09 -0300

    route: delete invitation

---
+++ apps/api/src/http/routes/invites/revoke-invite.ts
@@ -0,0 +1,62 @@
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { auth } from 'src/http/middlewares/auth'
+import { getUserPermissions } from 'src/utils/get-user-permissions'
+import { z } from 'zod'
+import { UnauthorizedError } from '../_errors/unauthorized-error'
+import { prisma } from 'src/lib/prisma'
+import { BadRequestError } from '../_errors/bad-request-error'
+
+export async function revokeInvite(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .post(
+      '/organizations/:slug/invites/:inviteId',
+      {
+        schema: {
+          tags: ['Invites'],
+          summary: 'Revoke a invite',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            slug: z.string(),
+            inviteId: z.string().uuid(),
+          }),
+          response: {
+            204: z.null(),
+          },
+        },
+      },
+      async (request, reply) => {
+        const { slug, inviteId } = request.params
+        const userId = await request.getCurrentUserId()
+        const { organization, membership } =
+          await request.getUserMembership(slug)
+
+        const { cannot } = getUserPermissions(userId, membership.role)
+
+        if (cannot('delete', 'Invite')) {
+          throw new UnauthorizedError(`You're not allowed to delete an invite.`)
+        }
+
+        const invite = await prisma.invite.findUnique({
+          where: {
+            id: inviteId,
+            organizationId: organization.id,
+          },
+        })
+
+        if (!invite) {
+          throw new BadRequestError('Invite not found.')
+        }
+
+        await prisma.invite.delete({
+          where: {
+            id: inviteId,
+          },
+        })
+
+        reply.code(204).send()
+      },
+    )
+}
