4c0fa7455483d43e8cbdac1adf5a03fd0fe1a58a
Kauane Vieira <kauaneviieira@gmail.com>
2025-09-14 17:06:51 -0300

    estados de erro do formulario

--- apps/web/src/app/auth/sign-in/actions.tsx
+++ apps/web/src/app/auth/sign-in/actions.tsx
@@ -1,20 +1,54 @@
 'use server'
 
+import { HTTPError } from 'ky'
+import { z } from 'zod'
+
 import { signInWithPassword } from "@/http/sign-in-with-password"
 
+const signInSchema = z.object({
+  email: z
+    .string()
+    .email({ message: 'Please, provide a valid e-mail address.' }),
+  password: z.string().min(1, { message: 'Please, provide your password.' }),
+})
+
+
 export async function signInWithEmailAndPassword(
-  previousState: unknown,
+  _: unknown,
   data: FormData,
 ) {
-  console.log(previousState)
-  const { email, password } = Object.fromEntries(data)
+  const result = signInSchema.safeParse(Object.fromEntries(data))
 
-  await new Promise((resolve) => setTimeout(resolve, 2000))
+  if (!result.success) {
+    const errors = result.error.flatten().fieldErrors
 
-  const result = await signInWithPassword({
-    email: String(email),
-    password: String(password),
-  })
+    return { success: false, message: null, errors }
+  }
 
-  return 'Sucesso!'
+  const { email, password } = result.data
+
+  try {
+    const { token } = await signInWithPassword({
+      email,
+      password,
+    })
+
+    console.log(token)
+  } catch (err) {
+    if (err instanceof HTTPError) {
+      const { message } = await err.response.json()
+
+      return { success: false, message, errors: null }
+    }
+
+    console.error(err)
+
+    return {
+      success: false,
+      message: 'Unexpected error, try again in a few minutes.',
+      errors: null,
+    }
+  }
+
+  return { success: true, message: null, errors: null }
 }
