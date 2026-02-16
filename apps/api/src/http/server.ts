de33760ab20f229f3ac22c83fd8d23f1f8af7f82
Kauane Vieira <kauanedev@gmail.com>
2026-02-16 19:29:45 -0300

    feat: add invite acceptance and retrieval routes with validation and permissions check

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -34,6 +34,9 @@ import { getMembers } from './routes/members/get-members'
 import { updateMember } from './routes/members/update-member'
 import { removeMember } from './routes/members/remove-member'
 import { createInvite } from './routes/invites/create-invite'
+import { getInvite } from './routes/invites/get-invite'
+import { getInvites } from './routes/invites/get-invites'
+import { acceptInvite } from './routes/invites/accept-invite'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -98,6 +101,9 @@ app.register(updateMember)
 app.register(removeMember)
 
 app.register(createInvite)
+app.register(getInvite)
+app.register(getInvites)
+app.register(acceptInvite)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('HTTP server running!')
