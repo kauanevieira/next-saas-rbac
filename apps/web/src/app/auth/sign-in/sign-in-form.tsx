3b624879467773ba20d48148636d1ad9471a2967
Kauane Vieira <kauanedev@gmail.com>
2026-02-17 12:37:15 -0300

    feat: implement useFormState hook for form state management in sign-in process

--- apps/web/src/app/auth/sign-in/sign-in-form.tsx
+++ apps/web/src/app/auth/sign-in/sign-in-form.tsx
@@ -3,7 +3,6 @@
 import { AlertTriangle, Loader2 } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
-import { useState, useTransition, type FormEvent } from 'react'
 
 import githubIcon from '@/src/assets/github-icon.svg'
 import { Alert, AlertDescription, AlertTitle } from '@/src/components/ui/alert'
@@ -12,6 +11,7 @@ import { Input } from '@/src/components/ui/input'
 import { Label } from '@/src/components/ui/label'
 import { Separator } from '@/src/components/ui/separator'
 import { signInWithEmailAndPassword } from './action'
+import { useFormState } from '@/src/hooks/use-form-state'
 
 export function SignInForm() {
   // const [{ errors, message, success }, formAction, isPending] = useActionState(
@@ -19,32 +19,12 @@ export function SignInForm() {
   //   { success: false, message: null, errors: null }
   // )
 
-  const [isPending, startTransition] = useTransition()
-  const [{ success, message, errors }, setFormState] = useState<{
-    success: boolean
-    message: string | null
-    errors: Record<string, string[]> | null
-  }>({
-    success: false,
-    message: null,
-    errors: null,
-  })
-
-  async function handleSignIn(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault()
-
-    const form = event.currentTarget
-    const data = new FormData(form)
-
-    startTransition(async () => {
-      const state = await signInWithEmailAndPassword(data)
-
-      setFormState(state)
-    })
-  }
+  const [{ errors, message, success }, handleSubmit, isPending] = useFormState(
+    signInWithEmailAndPassword
+  )
 
   return (
-    <form onSubmit={handleSignIn} className="space-y-4">
+    <form onSubmit={handleSubmit} className="space-y-4">
       {success === false && message && (
         <Alert variant="destructive">
           <AlertTriangle className="size-4" />
