f5a34e1fe4b0eb1205264ec3e83b45f29041939b
Kauane Vieira <kauanedev@gmail.com>
2026-02-17 14:39:12 -0300

    feat: add middleware for organization cookie management and implement Home page structure

--- apps/web/src/components/organization-switcher.tsx
+++ apps/web/src/components/organization-switcher.tsx
@@ -12,14 +12,35 @@ import {
   DropdownMenuTrigger,
 } from './ui/dropdown-menu'
 import { getOrganizations } from '../http/get-organizations'
+import { cookies } from 'next/headers'
 
 export async function OrganizationSwitcher() {
+  const currentOrg = (await cookies()).get('org')?.value
+
   const { organizations } = await getOrganizations()
+
+  const currentOrganization = organizations.find(
+    (org) => org.slug === currentOrg
+  )
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className="focus-visible:ring-primary flex w-[168px] items-center gap-2 rounded p-1 text-sm font-medium outline-none focus-visible:ring-2">
-        <span className="text-muted-foreground">Select organization</span>
+        {currentOrganization ? (
+          <>
+            <Avatar className="mr-2 size-4">
+              {currentOrganization.avatarUrl && (
+                <AvatarImage src={currentOrganization.avatarUrl} />
+              )}
+              <AvatarFallback />
+            </Avatar>
+            <span className="truncate text-left">
+              {currentOrganization.name}
+            </span>
+          </>
+        ) : (
+          <span className="text-muted-foreground">Select organization</span>
+        )}
         <ChevronsUpDown className="text-muted-foreground ml-auto size-4" />
       </DropdownMenuTrigger>
       <DropdownMenuContent
