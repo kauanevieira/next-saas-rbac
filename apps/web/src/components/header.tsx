68de4102f980018721b57b629b7d616803de26be
Kauane Vieira <kauanedev@gmail.com>
2026-02-17 16:18:28 -0300

    feat: add ProjectSwitcher component and integrate react-query for project management

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -7,6 +7,7 @@ import { OrganizationSwitcher } from './organization-switcher'
 import { ability } from '../auth/auth'
 import { Separator } from './ui/separator'
 import { ThemeSwitcher } from './theme/theme-switcher'
+import { ProjectSwitcher } from './project-switcher'
 
 export async function Header() {
   const permissions = await ability()
@@ -24,7 +25,12 @@ export async function Header() {
 
         <OrganizationSwitcher />
 
-        {permissions?.can('get', 'Project') && <p>Project</p>}
+        {permissions?.can('get', 'Project') && (
+          <>
+            <Slash className="text-border size-3 -rotate-[24deg]" />
+            <ProjectSwitcher />
+          </>
+        )}
       </div>
 
       <div className="flex items-center gap-4">
