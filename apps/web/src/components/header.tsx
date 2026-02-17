79f574f06beb458da08e97512627722a53d25155
Kauane Vieira <kauanedev@gmail.com>
2026-02-17 14:24:49 -0300

    feat: add OrganizationSwitcher component with dropdown menu for organization selection

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image'
 
 import rocketseatIcon from '@/src/assets/rocketseat-icon.svg'
 import { ProfileButton } from './profile-button'
+import { Slash } from 'lucide-react'
+import { OrganizationSwitcher } from './organization-switcher'
 
 export function Header() {
   return (
@@ -12,6 +14,10 @@ export function Header() {
           className="size-6 dark:invert"
           alt="Rocketseat"
         />
+
+        <Slash className="text-border size-3 -rotate-[24deg]" />
+
+        <OrganizationSwitcher />
       </div>
 
       <div className="flex items-center gap-4">
