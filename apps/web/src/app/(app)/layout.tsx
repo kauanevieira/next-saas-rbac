76ad61f1c4ce57f6f87f3bbeb50f2af898aabb60
Kauane Vieira <kauanedev@gmail.com>
2026-02-17 16:05:04 -0300

    feat: add CreateOrganization page with sheet component and update layout structure

--- apps/web/src/app/(app)/layout.tsx
+++ apps/web/src/app/(app)/layout.tsx
@@ -1,20 +1,21 @@
 import { isAuthenticated } from '@/src/auth/auth'
-import { Header } from '@/src/components/header'
 import { redirect } from 'next/navigation'
 
 export default function AppLayout({
   children,
+  sheet,
 }: Readonly<{
   children: React.ReactNode
+  sheet: React.ReactNode
 }>) {
   if (!isAuthenticated()) {
     redirect('/auth/sign-in')
   }
 
   return (
-    <div className="space-y-4 py-4">
-      <Header />
-      <main className="mx-auto w-full max-w-[1200px]">{children}</main>
-    </div>
+    <>
+      {children}
+      {sheet}
+    </>
   )
 }
