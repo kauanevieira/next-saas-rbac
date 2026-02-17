b65e02099d72bc337d2b567dc010b759325b14c3
Kauane Vieira <kauanedev@gmail.com>
2026-02-17 15:20:33 -0300

    feat: add theme switcher component and integrate with layout for theme management

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -5,6 +5,8 @@ import { ProfileButton } from './profile-button'
 import { Slash } from 'lucide-react'
 import { OrganizationSwitcher } from './organization-switcher'
 import { ability } from '../auth/auth'
+import { Separator } from './ui/separator'
+import { ThemeSwitcher } from './theme/theme-switcher'
 
 export async function Header() {
   const permissions = await ability()
@@ -26,6 +28,8 @@ export async function Header() {
       </div>
 
       <div className="flex items-center gap-4">
+        <ThemeSwitcher />
+        <Separator orientation="vertical" className="h-5" />
         <ProfileButton />
       </div>
     </div>
